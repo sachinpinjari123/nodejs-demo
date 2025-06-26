cdce8733ff1ca5264e43c7ad554660cc78fd3224
sachinpinjari123 <sachinpinjari2@gmail.com>
2025-06-26 07:07:35 -0400

    App Changes2

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
+// Serve static files from 'public' folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-    res.send('Hello, this is an UPDATED version from OpenShift CI/CD Pipeline!');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
